d66e110bdf9a35e871447e649ce6d179407a458a
PapajONse <88533181+PapajONse@users.noreply.github.com>
2023-07-30 23:29:20 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -17,14 +17,7 @@ $$  /   \$$ |\$$$$$$$\ $$$$$$$  |$$$$$$$  |  \$$$$  |\$$$$$$$\ $$ |
 
 local UserGroup
 
-ESX = nil
-
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-end)
+ESX = exports['es_extended']:getSharedObject()
 
 RegisterNetEvent('sendProximityMessage')
 AddEventHandler('sendProximityMessage', function(id, name, message)
